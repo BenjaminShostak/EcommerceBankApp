9709d18da2bc8f658b335c3215d438aceb72e9ba
Benjamin Shostak <benshostak@rocketmail.com>
2020-12-20 17:04:10 -0800

    added express framework nand package json

---
+++ express.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+// ===========================================================
+
+const app = express();
+var PORT = 3000;
+
+// Start our server
+server.listen(PORT, function() {
+    // Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
+  // Create a function which handles incoming requests and sends responses
+  function handleRequest(req, res) {
+  
+    // Capture the url the request is made to
+    var path = req.url;
+ 
+};
+ 
