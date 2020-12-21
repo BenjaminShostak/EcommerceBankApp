f25a4267cae912375e2c1e11b90f83f4d47d4b7a
Benjamin Shostak <benshostak@rocketmail.com>
2020-12-20 17:27:30 -0800

    can start server

--- express.js
+++ express.js
@@ -1,22 +1,36 @@
 const express = require('express')
 const app = express()
+var path = require("path");
+var http = require("http");
+
+
 
 // ===========================================================
 
-const app = express();
 var PORT = 3000;
 
+
+
 // Start our server
-server.listen(PORT, function() {
-    // Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function() {
+
+    // Callback triggered when server is successfully listening. Hurray
+    console.log("App listening on PORT " + PORT);
   });
   
+  
+
+  // Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
   // Create a function which handles incoming requests and sends responses
   function handleRequest(req, res) {
-  
+    response.end("It Works!! Path Hit: " + request.url);
+};
+var server = http.createServer(handleRequest);
     // Capture the url the request is made to
     var path = req.url;
  
-};
+
  
