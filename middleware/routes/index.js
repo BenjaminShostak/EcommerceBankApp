85da0024a4a1788f0cdb603ff930f1094d304d86
Benjamin Shostak <benshostak@rocketmail.com>
2021-03-03 21:36:27 -0800

    changed css

--- middleware/routes/index.js
+++ middleware/routes/index.js
@@ -3,8 +3,19 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+
+
+
+//do this here_____________________
+
+
+
+
+
+
+
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Way!!!</h1>")
 });
 
 module.exports = router;
